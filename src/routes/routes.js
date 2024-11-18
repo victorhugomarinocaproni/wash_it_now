e8e3098ed27bface8969fc0cd7097f55004ba215
Victor Caproni <victorcaproni04@gmail.com>
2024-11-18 13:40:44 -0300

    Middleware criado para validar sessão do Usuário. Middleware bloqueia páginas e redireciona o Usuário para Login caso a sessão não esteja validada.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,8 @@
 const { Router } = require('express');
 const router = Router();
-
 const RouteController = require('../controllers/RouteController');
+const { requireAuth } = require('../middleware/authMiddleware');
 
-router.get('/home', RouteController.home_get);
+router.get('/home', requireAuth, RouteController.home_get);
 
 module.exports = router;
