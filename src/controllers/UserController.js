f0538cfa13111576d4878d052843858608efea7f
Victor Caproni <victorcaproni04@gmail.com>
2024-11-23 16:07:14 -0300

    Atualização dos Dados do Usuário funcionando.

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const Usuario = require('../database/models/Usuario');
+const bcrypt = require('bcrypt');
 
 module.exports = {
 
@@ -7,7 +8,7 @@ module.exports = {
         const usuario = await Usuario.findByPk(usuario_id);
 
         if (usuario) {
-            res.status(201).json({ user: usuario });
+            res.status(201).render('update', { usuario });
         }
         else {
             res.status(400).json({ err: 'Usuário não encontrado'});
@@ -16,5 +17,47 @@ module.exports = {
 
     async atualizar_dados_put(req, res) {
         const usuario_id = req.params.id;
+        const usuario = await Usuario.findByPk(usuario_id);
+
+        if (usuario) {
+
+            if (usuario.senha === req.body.senha) {
+                const usuario = await Usuario.update({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    endereco: req.body.endereco,
+                    numero_endereco: req.body.numero_endereco,
+                    zipcode: req.body.zipcode,
+                },
+                {
+                    where: {id: usuario_id}
+                }
+                );
+            }
+            else {
+                const salt = await bcrypt.genSalt(10);
+                const senha = await bcrypt.hash(req.body.senha, salt); 
+
+                const usuario = await Usuario.update({
+                    nome: req.body.nome,
+                    email: req.body.email,
+                    senha: senha,
+                    endereco: req.body.endereco,
+                    numero_endereco: req.body.numero_endereco,
+                    zipcode: req.body.zipcode,
+                },
+                {
+                    where: {id: usuario_id}
+                }
+                );
+            }
+
+        }
+        else {
+            res.status(400).json({ err: 'Usuário não encontrado'});
+        }
+
+
+
     },
 };
